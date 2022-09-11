95cde030b315e3e9db10c11f5bf0e99e6812a8e8
Matin-Sangabi <89559512+Matin-Sangabi@users.noreply.github.com>
2022-09-11 15:24:47 +0430

    products updated

--- models/products.js
+++ models/products.js
@@ -24,7 +24,14 @@ const productSchema = new mongoose.Schema(
         },
       },
     ],
-    
+    description: [
+      {
+        content: {
+          title: { type: String, required: true },
+          body: { type: String, required: true },
+        },
+      },
+    ],
     price: { type: Number, required: true },
     offPrice: { type: Number, required: true },
     discount: { type: Number, default: 0 },
